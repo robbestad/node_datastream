8d69234a34208a67a3171bdbd1d46262be92dd5b
Sven Anders Robbestad <robbestad@gmail.com>
2020-10-27 07:56:18 +0100

    pipe to stdout

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,16 @@ import * as fs from "fs";
 import * as path from "path";
 
 const parser = async () => {
-  let file = path.resolve(__dirname, "../data.json");
-  let stream = fs
-    .createReadStream(file)
-    .pipe(JSONStream.parse("*"))
-    .pipe(
-      es.map(async(data: any, callback: Function) => {
-        let out:any = data.userId < 10 ? data : null;
-        callback(null, out);
-        debugger;
-      })
-    );
-    stream.on("error", (data: any) => console.log("->error: ",data))
-    stream.on("data", (data: any) => data && console.log("->",(data)))
-    stream.on("finish", () => console.log("stream done"));
-    return stream;
+    let file = path.resolve(__dirname, "../data.json");
+    let inspect = require("util").inspect;
+    return fs
+        .createReadStream(file)
+        .pipe(JSONStream.parse("*"))
+        .pipe(es.map(function (data: any, cb: Function) {
+            cb(null
+                , (data.userId === 5 ? inspect(data) : ""));
+        }))
+        .pipe(process.stdout);
 };
 
-parser();
+parser()
